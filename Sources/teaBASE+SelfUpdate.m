27974d56e86cb2ea13e021709fd7dfc777c2e3b8
Max Howell <mxcl@me.com>
2024-12-30 11:21:24 -0500

    Do a naive semver check so I can test rcs

--- Sources/teaBASE+SelfUpdate.m
+++ Sources/teaBASE+SelfUpdate.m
@@ -23,7 +23,7 @@
         
         NSString *latest_version = json[@"tag_name"];
         
-        if ([latest_version isEqualToString:current_version]) return;
+        if (!semver_is_greater(latest_version, current_version)) return;
         
         id fmt = [NSString stringWithFormat:@"-%@.dmg", [latest_version substringFromIndex:1]];
         for (NSDictionary *asset in json[@"assets"]) {
@@ -41,11 +41,42 @@ void download(id url, teaBASE *self) {
 #if DEBUG
     NSLog(@"teaBASE: would update to: %@", url);
 #else
+    NSLog(@"teaBASE: updating to: %@", url);
+    
     id bundle_path = [[NSBundle bundleForClass:[self class]] bundlePath];
     id script_path = [bundle_path stringByAppendingPathComponent:@"Contents/Scripts/self-update.sh"];
 
     run(script_path, @[url, bundle_path], nil);
 #endif
+}
+
+// naive compare that ignores pre-release ids etc.
+BOOL semver_is_greater(NSString *version1, NSString *version2) {
+    // Remove leading 'v' if present
+    if ([version1 hasPrefix:@"v"]) {
+        version1 = [version1 substringFromIndex:1];
+    }
+    if ([version2 hasPrefix:@"v"]) {
+        version2 = [version2 substringFromIndex:1];
+    }
+    
+    NSArray<NSString *> *components1 = [version1 componentsSeparatedByString:@"."];
+    NSArray<NSString *> *components2 = [version2 componentsSeparatedByString:@"."];
+
+    NSInteger maxLength = MAX(components1.count, components2.count);
+
+    for (NSInteger i = 0; i < maxLength; i++) {
+        NSInteger value1 = i < components1.count ? [components1[i] integerValue] : 0;
+        NSInteger value2 = i < components2.count ? [components2[i] integerValue] : 0;
+
+        if (value1 > value2) {
+            return YES;
+        } else if (value1 < value2) {
+            return NO;
+        }
+    }
+
+    return NO;
 }
 
 @end
